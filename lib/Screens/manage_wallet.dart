203e2e947fb586d3a7b2a0e7c3aec4de9cd3d8df
Awaizkazi <Owaizkazi789@gmail.com>
2023-12-25 22:44:29 +0530

    gradinet background Color applied to Manage wallet Screens

--- lib/Screens/manage_wallet.dart
+++ lib/Screens/manage_wallet.dart
@@ -7,53 +7,62 @@ class ManageWalletScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        title: const Text('Manage Wallet'),
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [
+          ColorConstants.backgroundColorDarkBlue,
+          Colors.black,
+          ColorConstants.backgroundColorPurple,
+        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
       ),
-      backgroundColor: Colors.black,
-      body: ListView.separated(
-        separatorBuilder: (context, index) => SizedBox(height: 0.5.h),
-        padding: EdgeInsets.symmetric(horizontal: 15.w, vertical: 20.h),
-        itemCount: theme_data.length,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            dense: true,
-            horizontalTitleGap: 0,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(index == 1
-                        ? 0
-                        : index == 2
-                            ? 0
-                            : 10.sp),
-                    topRight: Radius.circular(index == 1
-                        ? 0
-                        : index == 2
-                            ? 0
-                            : 10.sp),
-                    bottomLeft: Radius.circular(index == 0
-                        ? 0
-                        : index == 1
-                            ? 0
-                            : 10.sp),
-                    bottomRight: Radius.circular(index == 0
-                        ? 0
-                        : index == 1
-                            ? 0
-                            : 10.sp))),
-            leading: Icon(
-              theme_data[index]["icon"],
-              color: Colors.white,
-            ),
-            title: Text(
-              theme_data[index]["title"],
-              style: const TextStyle(color: Colors.white, fontSize: 15),
-            ),
-            tileColor: ColorConstants.listtitleColor,
-          );
-        },
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: const Text('Manage Wallet'),
+        ),
+        backgroundColor: Colors.black,
+        body: ListView.separated(
+          separatorBuilder: (context, index) => SizedBox(height: 0.5.h),
+          padding: EdgeInsets.symmetric(horizontal: 15.w, vertical: 20.h),
+          itemCount: theme_data.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              dense: true,
+              horizontalTitleGap: 0,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(index == 1
+                          ? 0
+                          : index == 2
+                              ? 0
+                              : 10.sp),
+                      topRight: Radius.circular(index == 1
+                          ? 0
+                          : index == 2
+                              ? 0
+                              : 10.sp),
+                      bottomLeft: Radius.circular(index == 0
+                          ? 0
+                          : index == 1
+                              ? 0
+                              : 10.sp),
+                      bottomRight: Radius.circular(index == 0
+                          ? 0
+                          : index == 1
+                              ? 0
+                              : 10.sp))),
+              leading: Icon(
+                theme_data[index]["icon"],
+                color: Colors.white,
+              ),
+              title: Text(
+                theme_data[index]["title"],
+                style: const TextStyle(color: Colors.white, fontSize: 15),
+              ),
+              tileColor: ColorConstants.listtitleColor,
+            );
+          },
+        ),
       ),
     );
   }
